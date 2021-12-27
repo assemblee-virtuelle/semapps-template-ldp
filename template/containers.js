826df253f3b6b52097a667c2b02214129bf2e162
Niko <info@parlepeuple.fr>
2021-12-27 03:19:38 +0200

    adding /users container if webAcl

--- template/containers.js
+++ template/containers.js
@@ -2,6 +2,13 @@ module.exports = [
   {
     path: '/'
   },
+{{#webAcl}}  
+  {
+    path: '/users',
+    acceptedTypes: ['pair:Person', ACTOR_TYPES.PERSON],
+    dereference: ['sec:publicKey', 'pair:hasLocation/pair:hasPostalAddress']
+  },
+{{/webAcl}}
   {
     path: '/files',
     // TODO load all images with tokens so files can be hidden by default
